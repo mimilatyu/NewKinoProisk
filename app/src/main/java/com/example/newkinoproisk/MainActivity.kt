dbefa33fb389492eeb92d156d89a7e24b9607257
mimilatyu <turmyshov@mail.ru>
2022-10-11 17:37:25 +0300

    made base screen

---
+++ app/src/main/java/com/example/newkinoproisk/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.newkinoproisk
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.newkinoproisk.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
